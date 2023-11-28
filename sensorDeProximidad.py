dcd4be42cf1e01b19062a9c05e11dee45857bdeb
beludominguezz <126901573+beludominguezz@users.noreply.github.com>
2023-11-28 14:02:09 -0300

    Update sensorDeProximidad.py

--- sensorDeProximidad.py
+++ sensorDeProximidad.py
@@ -1,25 +1,17 @@
 from machine import Pin
 import utime
 
-# Define the GPIO pin numbers for the trigger and echo pins
-#ECHO_PIN = 20
-#TRIGGER_PIN = 21
-
-# Initialize trigger and echo pins
 trigger = Pin(20, Pin.OUT)
 echo = Pin(21, Pin.IN)
 
 def measure_distance():
-    # Ensure trigger is low initially
     trigger.low()
     utime.sleep_us(2)
 
-    # Send a 10 microsecond pulse to the trigger pin
     trigger.high()
     utime.sleep_us(5)
     trigger.low()
      
-  # Measure the duration of the echo pulse (in microseconds)
     #pulse_duration = time_pulse_us(echo, Pin.high)
 
     while echo.value() == 0:
@@ -34,10 +26,7 @@ def measure_distance():
     distance = "{:.2f}".format(distance)
     
     print(distance + " cm")
-    return str(distance)
-
-    # Wait for 1 second before taking the next measurement
-    
+    return str(distance)    
 
 
 
