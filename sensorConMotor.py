3b019027e38e39f42325a8e9979498f84bd6b54a
beludominguezz <126901573+beludominguezz@users.noreply.github.com>
2023-11-28 13:26:58 -0300

    Update sensorConMotor.py

--- sensorConMotor.py
+++ sensorConMotor.py
@@ -58,16 +58,10 @@ def measure_distance():
     # el trigger va low al comienzo
     trigger.low()
     sleep_us(2)
-
-    # Send a 10 microsecond pulse to the trigger pin
     trigger.high()
     sleep_us(10)
     trigger.low()
-
-    # Measure the duration of the echo pulse (in microseconds)
     pulse_duration = time_pulse_us(echo, Pin.high)
-
-    # Calculate the distance (in centimeters) using the speed of sound (343 m/s)
     distance = pulse_duration * 0.0343 / 2
     return distance
 
